afbbb9bbd0d5a66867aba05c6bf59a5b2c72444b
jayBpop <86476698+jayBpop@users.noreply.github.com>
2022-08-15 12:46:38 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,21 +1,3 @@
-def buildJar() {
-    echo "Building the application"
-    sh 'mvn clean'
-    sh 'mvn package'
-} 
-
-def buildImage() {
-    echo "Building Docker Image"
-    sh 'docker build -t hdevop/myrepo:firstjavaapp-2.1 .'
-    }
-
-def pushImage(){
-    echo "Pushing the image to docker hub"
-    withCredentials([usernamePassword(credentialsId:'docker-hub-repo', passwordVariable:'password', usernameVariable:'username')])
-            {
-                  sh 'echo $password | docker login -u $username --password-stdin'
-                  sh 'docker push hdevop/myrepo:firstjavaapp-2.1'
-            }
 
 }
 def deployApp() {
